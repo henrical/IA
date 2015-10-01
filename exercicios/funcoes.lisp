e3281b4f858dfa8da3de64cf7ab1c9c5694f49d0
henrical <henrique.cal94@gmail.com>
2015-10-01 18:11:05 +0100

    many more functions

--- exercicios/funcoes.lisp
+++ exercicios/funcoes.lisp
@@ -1,20 +1,82 @@
 ;;  1.3
 (defun findmax(el1 el2)
 	(if (> el1 el2)
-	      (+ 0 el1)
-		(+ 0 el2)
+	    el1
+	    el2
 	)
 )
 
 ;; 1.4
 ;; 1.4.1, findmax3 usando IF's
-(defun findmax3(el1 el2 el3)
-	(if (> el1 el2 el3)
-		(+ 0 el1)
-		(
-			(if (> el1 el3 el2)
-				(+ 0 el1)
-			)
+(defun findmax3-if(el1 el2 el3)
+	(let ((mx)) 			;;define variavel mx
+		(setf mx el1)           ;;atribui a mx o valor de el1
+		(if (< mx el2)          
+		    (setf mx el2)       ;;mx passa a ter o valor de el2, caso a condiçao se verifique
+		    ()			;;em caso falso, retorna NIL
 		)
+		(if (< mx el3)
+		    (setf mx el3)
+		    () 			;;usar macro WHEN em vez de IF, para nao ter de escrever esta linha
+		)
+		mx               		;;retorna mx
 	)
 )
+
+;; 1.4.2, findmax3 usando COND's
+(defun findmax3-cond(el1 el2 el3)
+	(let ((mx))
+		(setf mx el1)
+		(cond ((< mx el2)(setf mx el2))
+		)
+		(cond ((< mx el3)(setf mx el3))
+		)
+		mx
+	)
+)
+
+;; 1.4.2, findmax3 usando 1.3
+(defun findmax3-call(el1 el2 el3)
+	(let ((mx))
+		(setf mx (findmax el1 el2))
+		(when (< mx el3)
+			(setf mx el3)
+		)
+		mx
+	)
+)
+
+;; 1.5
+;; (defun soma(el1 el2)
+;; 	(if(zerop el2)
+;; 	   nil
+;; 	   ( 
+;; 	   )
+;; 	)
+;; )
+
+;; 1.6
+(defun potencia(base expoente)
+	(if (= expoente 1)
+	    base
+	    (* base (potencia base (1- expoente))) ;; base * potencia(base,expoente-1)
+	)
+)
+
+;; 1.8
+(defun soma-1(lista)
+	(if (null lista)
+	    ()
+	    (cons (1+ (first lista)) (soma-1 (rest lista)))
+	)
+)
+
+;; 1.9
+(defun soma-n(n lista)
+	(if (null lista)
+	    ()
+	    (cons (+ n (first lista)) (soma-n n (rest lista)))
+	)
+)
+
+
